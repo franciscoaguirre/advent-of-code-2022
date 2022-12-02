f089c2b1d84b7f6dfe17f3d9ec72fa1e0f1affa3
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2022-12-02 15:53:51 -0300

    Add skeleton

---
+++ src/days/day_01/mod.rs
@@ -0,0 +1,13 @@
+mod exercise_1;
+mod exercise_2;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn solve(input_file: File) {
+    todo!();
+}
+
+fn parse_input(input_file: File) {
+    todo!();
+}
---
+++ src/days/mod.rs
@@ -0,0 +1,14 @@
+mod day_01;
+
+use std::fs::File;
+
+pub fn solve_day(day: u32) {
+    let input_file =
+        File::open(format!("inputs/day_{:02}.txt", day)).expect("Input file not found");
+
+    match day {
+        1 => day_01::solve(input_file),
+        26..=u32::MAX => panic!("Advent of Code does not have that many days!"),
+        _ => panic!("No solution found for that day yet!"),
+    }
+}
